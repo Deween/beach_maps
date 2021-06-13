ec02d83800fa5d0f5dc627eb8eb93ce111f118d7
Deween Piyasena <deweenloji@gmail.com>
2021-06-12 23:30:04 -0400

    Changing maps based on location

--- public/main.js
+++ public/main.js
@@ -18,15 +18,20 @@ function nextImage() {
   slideshowImages[currentImageCounter].style.opacity = 1;
 }
 
+let mapPlacementLat= 42.3601 
+let mapPlacementLng =-71.0598;
+let address;
 
+//MAP VIEWING
 function initMap(){
   //map options
-  let options_map= {
-    zoom:14,
-    center:{lat:42.3601, lng:-71.0598}
-  }
-  //new map 
-  let map = new google.maps.Map(document.getElementById('map'),options_map);
+  
+  // let options_map= {
+  //   zoom: 14,
+  //   center:{lat: mapPlacementLat, lng: mapPlacementLng}
+  // }
+  // //new map 
+  // let map = new google.maps.Map(document.getElementById('map'),options_map);
 
   //Adding a marker 
   // let marker = new google.maps.Map({
@@ -34,8 +39,11 @@ function initMap(){
   //   map:map
   // });
   initAutocomplete();
+  
 }
 
+
+// AUTOCOMPLETE FUNCTIONALITY
 let autocomplete;
 function initAutocomplete(){
   autocomplete = new google.maps.places.Autocomplete(
@@ -43,7 +51,7 @@ function initAutocomplete(){
     {
       types: ['establishment'],
       componentRestrictions: {'country':['CA','USA']},
-      fields: ['place_id','name','geometry']
+      fields: ['place_id','name','geometry','adr_address']
   
     }
   )
@@ -59,8 +67,38 @@ function onPlaceClick(){
     document.getElementById('autocomplete').placeholder = "Search";
   } else {
     //Show details of the valid place
-    document.getElementById('details').innerHTML = place.name;
+    // document.getElementById('details').innerHTML = place.adr_address;
+    address = place.adr_address;
+    geocode(address)
   }
-  
 
 }
+
+function geocode(location){
+  // let location = ' 22 Main st Bostan MA';
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params:{
+      address:location,
+      key: 'AIzaSyANso2rcGXQQY850G3SgTWtBc5Vs26C1Pg'
+    }
+  })
+  .then(function(result){
+    // console.log(result);
+  
+    mapPlacementLat = (result.data.results[0].geometry.location['lat']);
+    mapPlacementLng = (result.data.results[0].geometry.location['lng']);
+    console.log("Latitude: " + mapPlacementLat + " Longi: " + mapPlacementLng)
+    let options_map= {
+        zoom: 14,
+        center:{lat: mapPlacementLat, lng: mapPlacementLng}
+      }
+      //new map 
+    let map = new google.maps.Map(document.getElementById('map'),options_map);
+    
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+
